78670904a1792078fbfc95b2db6eb285ec08a0d7
softalent <anishin523@yandex.com>
2016-10-01 22:07:44 +0300

    delete_update

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,9 +1,31 @@
 import React, {Component,PropTypes} from 'react';
+import {Tasks} from '../api/tasks.js';
 
 export default class Task extends Component{
+	toggleChecked(){
+		Tasks.update(this.props.task._id,{
+			$set:{checked: !this.props.task.checked}
+		})
+	}
+
+	deleteThisTask(){
+		Tasks.remove(this.props.task._id);
+	}
 	render(){
+		const taskClassName=this.props.task.checked?'checked':'';
 		return(
-			<li>{this.props.task.text}</li>
+			<li className={taskClassName}>
+				<button className='delete' onClick={this.deleteThisTask.bind(this)}>
+				&times;
+				</button>
+				<input 
+					type='checkbox'
+					readOnly
+					checked={this.props.task.checked}
+					onClick = {this.toggleChecked.bind(this)}
+				/>
+				<span className='text'>{this.props.task.text}</span>
+			</li>
 			);
 	}
 };
