5d8c653b982c066b3312a3d4c6f168040a95f528
softalent <anishin523@yandex.com>
2016-10-02 10:28:07 +0300

    remove and filtering completed task added

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,12 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			hideCompleted:false
+		}
+	}
 	handleSubmit(event){
 		event.preventDefault();
 		const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
@@ -17,8 +23,25 @@ class App extends Component{
 		});
 		ReactDOM.findDOMNode(this.refs.textInput).value='';
 	}
+
+	toggleHideCompleted(event){
+		//event.preventDefault();
+		this.setState({
+			hideCompleted: !this.state.hideCompleted
+		});
+	}
+
 	renderTasks(){
-		return this.props.tasks.map((task)=>(
+		let filteredTasks = this.props.tasks;
+		console.log('before filter');
+		console.log(filteredTasks);
+		// console.log(task.checked);
+		if(this.state.hideCompleted){
+			filteredTasks = filteredTasks.filter(task => !task.checked);
+		}
+		console.log('after filtered');
+		console.log(filteredTasks);
+		return filteredTasks.map((task)=>(
 			<Task key={task._id} task={task}/>
 		));
 	}
@@ -27,6 +50,15 @@ class App extends Component{
 			<div className="container">
 				<header>
 					<h1>Todo List</h1>
+					<label className='hide-completed'>
+						<input 
+							type='checkbox'
+							readOnly
+							checked={this.state.hideCompleted}
+							onClick={this.toggleHideCompleted.bind(this)}
+						/>
+							Hide Completed Tasks
+					</label>
 					<form className='new-task' onSubmit={this.handleSubmit.bind(this)} >
 						<input
 							type = 'text'
